6dc16e5fd0c98a7f50ee81411428b0caf965c350
ryanwetzstein <ryan.wetzstein@ndus.edu>
2025-03-03 11:52:04 -0600

    added property binding to homecomponent template

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import { Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
 
