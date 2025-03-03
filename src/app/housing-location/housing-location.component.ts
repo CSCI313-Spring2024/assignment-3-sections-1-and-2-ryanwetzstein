5765d52c4a743cede1e63ddd1cc2973c9c20db68
ryanwetzstein <ryan.wetzstein@ndus.edu>
2025-03-03 12:14:22 -0600

    added details page

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink, RouterModule, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css',
 })
